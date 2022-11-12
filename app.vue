6916ca1a1b2d6c4e46bb1d089b839755e24153eb
Diego Costa <diego.machado.d.costa@gmail.com>
2022-11-11 23:57:50 -0300

    feat: add layout

--- app.vue
+++ app.vue
@@ -1,9 +1,14 @@
 <template lang="pug">
 div
-  h1 I'm using pug!
+  header
+    span Header!
+  main
+    nuxt-page
+  footer
+    span Footer!
 </template>
 
 <style lang="styl" scoped>
 h1
-    color green
+  color green
 </style>
